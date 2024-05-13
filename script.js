f512aede34c177765ba44e297f6c0c9a5288b903
Anosha-Ashraf <160060320+Anoshaa@users.noreply.github.com>
2024-05-13 15:53:43 +0500

    Js

---
+++ script.js
@@ -0,0 +1,145 @@
+// index ki file may show hoga
+ document.write("Hello");
+
+
+ var first_name = "anoshaashrafkhan";
+ console.log(first_name);
+ var lastname = "khan";
+ console.log(lastname);
+
+var dateofBirth;
+dateofBirth = "31-oct-2007";
+ console.log(dateofBirth);
+
+
+// concordinate
+ var name = "Anosha Ashraf Khan";
+ var age = "16";
+ var email = "anoshaashrafkhan@gmail.com";
+ console.log(" My name is " + name + " My age is " + age + " My email is "+ email);
+
+
+// alert
+ var message = "Good Morning";
+ alert(message)
+
+
+// undefined
+ var hello;
+ console.log(hello);                                    
+
+
+
+var a = 10;
+ var b = 5;
+ var result = a * b;
+ console.log("multiply: " + result);
+
+
+var p = 12;
+ var q = 2;
+ console.log("the Add:" + (p + q));
+ console.log("the multiply:" + (p * q));
+
+
+
+ var originalNum = 23; 
+  var numToBeAdded = 7; 
+ var newNum = originalNum + numToBeAdded ;
+ console.log(originalNum + numToBeAdded);
+
+
+ //task
+ let var1 = "one"; var2 = "two"; var3 = "three";
+
+
+// // legal
+// // let FirstName;
+// // let lastname;
+// // let lastName;
+
+// // illegal
+// // let 12;
+// // let 12hello;
+// // let @34return;
+
+
+// example question
+ var costOfTicket = 600;
+ var buyingTicket = 5;
+ var answer = costOfTicket * buyingTicket;
+ console.log("total cost of ticket:" + answer );
+
+
+// pre increment
+var num = 2;
+const ans = ++num + 1;
+console.log (ans);
+
+var num = 2;
+var newNum = num++ + 1;
+console.log (newNum);
+
+
+// arthemetic operator
+var answer = 10 + 10 - 20 *5;
+console.log(answer);
+
+// concardinate
+
+ var name = "Anosha";
+ var email ="anosha@gmail.com"
+alert( "anosha" + "    "+"email" );
+
+ // prompt                                                                                                                                                                                                                              
+var spec = prompt("your, name is");
+ console.log(spec);
+
+var number1= +prompt("enter number",20);
+var number2= +prompt("enter second number");
+console.log(number1 + number2);
+
+// Assignment operator
+var x =20;
+x+= 5;
+console.log(x);
+
+ var c = 15;
+ var d = 10;
+ console.log(c==d);
+
+ let y = 15;
+ let z = "10";
+ console.log(y===z);
+
+
+
+// IF
+var mode ="light";
+var color = "black";
+if(mode == "light"){
+    console.log(color);
+}
+else{
+    console.log("white");
+}
+
+// 2 IF
+var age ="18";
+if(age >= 18){
+    console.log("you are eligible for voting");
+}
+else{
+    console.log("you are not eligible for voting");
+}
+
+// 3 IF
+var number = +prompt("enter any number");
+if(number % 2 == 0){
+    console.log(number, "is a even number");
+}
+else{ 
+    console.log(number, "is a odd number");
+}
+
+
